fff0f873995847dc10b9fb3f433bf0550a094c79
murattuver <murattuver@gmail.com>
2019-11-24 01:02:42 +0300

    Modified Player Class

--- Player.py
+++ Player.py
@@ -7,6 +7,11 @@ class Player:
 
 	def __init__(self, snake):
 		self.snake = snake
+		self.x = 0
+		self.y = 0
+		self.direction = [1, 0, 0, 0]
+		self.is_dead = False
+		self.did_eat = False
 
 	def move(self):
 		self.snake.MoveKeyDown(K_RIGHT)
