634384adbf1ea2ce0b5fa9249fa565f6b44ec3a4
murattuver <murattuver@gmail.com>
2019-11-24 14:02:09 +0300

    Egom sov with plot

--- PyMan.py
+++ PyMan.py
@@ -10,6 +10,8 @@ import MapManager
 from random import randint
 import numpy as np
 import time
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
@@ -33,6 +35,12 @@ if not pygame.font:
 
 BLOCK_SIZE = 24
 IS_AI = True
+
+def plot_seaborn(array_counter, array_score):
+    sns.set(color_codes=True)
+    ax = sns.regplot(np.array([array_counter])[0], np.array([array_score])[0], color="b", x_jitter=.1, line_kws={'color':'green'})
+    ax.set(xlabel='games', ylabel='score')
+    plt.show()
 
 class PyManMain:
 	"""The Main PyMan Class - This class handles the main
@@ -78,6 +86,8 @@ class PyManMain:
 
 
 	def main_loop(self):
+		score_plot = []
+		counter_plot = []
 		"""This is the Main Loop of the Game"""
 		while 1:
 			"""Load All of our Sprites"""
@@ -100,19 +110,69 @@ class PyManMain:
 							if ((event.key == K_RIGHT) or (event.key == K_LEFT) or (event.key == K_UP)
 									or (event.key == K_DOWN)):
 								self.pacman.MoveKeyDown(event.key)
+				# #self.pacman_sprites.update(self.block_sprites)
+				#
+				# # Not rendering ghost for now.
+				# # TODO: Render Ghosts.
+				# # self.ghost_sprites.update(self.block_sprites)
+				# # self.ghost2_sprites.update(self.block_sprites)
+				# # self.ghost3_sprites.update(self.block_sprites)
+				# # self.ghost4_sprites.update(self.block_sprites)
+				#
+				# if not (not pygame.sprite.collide_rect(self.ghost, self.pacman) and not pygame.sprite.collide_rect(
+				# 		self.ghost2,
+				# 		self.pacman)) or pygame.sprite.collide_rect(
+				# 	self.ghost3, self.pacman) or pygame.sprite.collide_rect(self.ghost4, self.pacman):
+				# 	self.collisions += 1
+				# 	print("Col+1", self.collisions)
+				# 	if self.collisions == self.collisiontol:
+				# 		self.game_counter += 1
+				# 		self.isGameOver = True
+				# 		print("gameover", self.game_counter)
+				# 		if IS_AI:
+				# 			self.learner.replay_new(self.learner.memory)
+				# 		break
+				# else:
+				# 	self.collisions = 0
+				#
+				# """Check for a snake collision/pellet collision"""
+				# lstCols = pygame.sprite.spritecollide(self.pacman, self.pellet_sprites, True)
+				# """Update the amount of pellets eaten"""
+				# self.pacman.pellets = self.pacman.pellets + len(lstCols)
+				# self.score = self.pacman.pellets * 10
+				#
+				#
+				# """Do the Drawing"""
+				# self.screen.blit(self.background, (0, 0))
+				# if pygame.font:
+				# 	font = pygame.font.Font(None, 36)
+				# 	text = font.render("Score %s" % self.score, 1, (255, 255, 255))
+				# 	textpos = text.get_rect(x=0)
+				# 	self.screen.blit(text, textpos)
+				#
+				# self.pellet_sprites.draw(self.screen)
+				# self.pacman_sprites.draw(self.screen)
+				# self.ghost_sprites.draw(self.screen)
+				# self.ghost2_sprites.draw(self.screen)
+				# self.ghost3_sprites.draw(self.screen)
+				# self.ghost4_sprites.draw(self.screen)
+				# pygame.display.flip()
+				# clock.tick(40)
 				else:
-					if self.game_counter < 20 and not self.isGameOver:
+					if self.game_counter < 25 and not self.isGameOver:
 						self.end = time.time()
 						self.elapsed_time = self.end-self.start
 						if(self.elapsed_time > 5):
 							print("sure biti")
 							self.start = time.time()
+							counter_plot.append(self.game_counter)
+							score_plot.append(self.score)
 							self.game_counter += 1
 							self.isGameOver = True
 							break
 						print(self.end-self.start)
 						print("baslıyoruz kızlar")
-						self.learner.epsilon = 80 - self.game_counter
+						self.learner.epsilon = 60 - self.game_counter
 						old_state = self.learner.get_state(self.map_manager, self.pacman)
 						print("basladık")
 						if randint(0, 200) <self.learner.epsilon:
@@ -179,6 +239,9 @@ class PyManMain:
 				print("buraya geldıysen serefsızsın")
 				self.pacman_sprites.update(self.block_sprites)
 				self.isGameOver = False
+
+				if (self.game_counter >= 25):
+					plot_seaborn(counter_plot, score_plot)
 				"""Update the sprites"""
 				# #self.pacman_sprites.update(self.block_sprites)
 				#
